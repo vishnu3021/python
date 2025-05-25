af444fc2f822eb21a360e830f522014bb8384491
vishnu3021 <vishnurrddy@gmail.com>
2025-05-25 16:57:53 +0530

    python day2 class

---
+++ day2.py
@@ -0,0 +1,31 @@
+#data types
+### 
+# 1)single value data type
+#integer
+#float
+#boolean
+#complex
+
+integer = 50
+float = 505.5800
+complex = 85+8j
+boolean = True
+print(type(integer))
+print(type(float))
+print(type(complex))
+print(type(boolean))
+
+### 2) collection data type
+#string: str is declared using sing , double, triple quotes.  single line str is declared by using single quote
+# triple quotes are used for multiple lines 
+#List 
+#tuple
+#set
+#dictionary
+str1 = 'single line string'
+print(str1)
+str2 = " hai this double quote str"
+print(str2)
+str3 = '''hello this is a 3 quote string '''
+print(str3)
+
