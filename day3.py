b25fd178c38be655694fbf57be5a13a57f5d6c80
vishnu3021 <vishnurrddy@gmail.com>
2025-05-26 19:33:54 +0530

    day3 python

---
+++ day3.py
@@ -0,0 +1,31 @@
+#List, Tuple,Set, Dictionary
+
+#1 List
+L=[20,501,90,55,5,5,10,5,10]
+L[-1]=66
+print(L)
+list=[20,20.5,5+6j,True,'str',{10,5},{'a':6, 'b':"hello"}]
+print(list)
+
+
+#tuple
+t=(5,58,6,1,55,58,10,10,"str1",5.2,[58,21],{51,5})
+#print(type(t))
+tuple=(1,5,3,4,2)
+print(tuple)
+print(t)
+
+#set
+
+set=set()
+print(type(set))
+
+s={2,2,6,5,5,1,2,3,4,5,6,'hello',True,5.2,5+6j}
+print(s)
+
+# print(type(s))
+
+#dictionary
+d={'a':30, 'b':10 }
+d['c']=20
+print(d)
